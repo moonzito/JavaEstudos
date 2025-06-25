c79f8d1d9f9b9555de758e3a64ce4791118aa0ae
Carlos Gabriel <moon_ita_mit_04@hotmail.com>
2025-06-24 23:39:10 -0300

    Create desafioX.java

---
+++ desafioX.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //Scanner leia = new Scanner(System.in);
+
+        for (int num = 10; num<=100; num +=10){
+            System.out.println(num);
+        }
+
+    }
+}
