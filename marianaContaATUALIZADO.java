51c8d13d96a7c51e4a1589ee1d4a1529ce12da7a
Carlos Gabriel <moon_ita_mit_04@hotmail.com>
2025-06-24 23:18:25 -0300

    Create marianaContaATUALIZADO.java

---
+++ marianaContaATUALIZADO.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//import java.lang.Math;
+public class Main {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        int cont = 0;
+
+        do {
+            cont++;
+            System.out.printf("Mariana conta %d.\n",cont);
+        } while (cont<10);
+
+    }
+}
