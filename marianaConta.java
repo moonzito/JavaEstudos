2889e398f0573587ccc76ef9e1801f8ac5ab19cb
Carlos Gabriel <moon_ita_mit_04@hotmail.com>
2025-06-24 22:45:04 -0300

    Create marianaConta.java

---
+++ marianaConta.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//import java.lang.Math;
+public class Main {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        int cod = 1;
+
+        while(cod<10){
+            System.out.printf("Mariana conta %d\n",cod);
+            cod++;
+        }
+
+    }
+}
