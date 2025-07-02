9d198c868c40528cd4aed59af917a8cf1fc9bf09
Carlos Gabriel <moon_ita_mit_04@hotmail.com>
2025-07-02 18:11:34 -0300

    Create nota.java

---
+++ nota.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class nota {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        float n1 = leia.nextFloat();
+        float n2 = leia.nextFloat();
+
+        System.out.print("A média do aluno é "+media.mediaA(n1,n2));
+
+    }
+}
